ccc0c26aa47e74b8a3992b91f7b5f9596a5cc273
Jack Desert <jdbip@riseup.net>
2019-08-29 08:38:34 -0700

    T-03 Systemd config

--- watcher.py
+++ watcher.py
@@ -1,5 +1,6 @@
 from twisted.internet import inotify
 from twisted.python.filepath import FilePath
+import os
 import pdb
 
 class Watcher:
@@ -7,15 +8,17 @@ class Watcher:
     # https://twistedmatrix.com/documents/current/api/twisted.internet.inotify.html
 
     UPDATE = 2
-    WATCHED_FILENAME = FilePath(b'/tmp/arscca-live.jinja2')
+    WATCHED_FILENAME = '/tmp/arscca-live.jinja2'
+    WATCHED_FILENAME_OBJECT = FilePath(WATCHED_FILENAME.encode())
 
     def __init__(self, callback):
         self.callback = callback
+        self._ensure_filename_exists()
 
     def watch(self):
         notifier = inotify.INotify()
         notifier.startReading()
-        notifier.watch(self.WATCHED_FILENAME, callbacks=[self._invoke_callback])
+        notifier.watch(self.WATCHED_FILENAME_OBJECT, callbacks=[self._invoke_callback])
 
     def _invoke_callback(self, ignored, filepath, mask):
         # Note you will end up in this method TWICE
@@ -23,6 +26,14 @@ class Watcher:
         if mask == self.UPDATE:
             print(f'file updated: {filepath}. Invoking callback {self.callback}')
             self.callback()
+
+    def _ensure_filename_exists(self):
+        if os.path.isfile(self.WATCHED_FILENAME):
+            return
+        with open(self.WATCHED_FILENAME, 'w', encoding='utf-8') as f:
+            f.write('<h1>Waiting for AxWare file to be uploaded</h1>')
+
+        
 
 
 if __name__ == '__main__':
